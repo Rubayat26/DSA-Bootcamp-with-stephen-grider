be33a2fdf25d557260c566cd8c629707794fe627
Rubayat Islam <rubayat10me@gmail.com>
2022-06-13 00:24:59 -0400

    solved anagrams using charMap

--- anagrams/index.js
+++ anagrams/index.js
@@ -9,7 +9,58 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-    let sortedStringA = shortenSortedString(stringA);
+
+    let charMapA = characterMapString(stringA);
+    let charMapB = characterMapString(stringB);
+
+    if (Object.keys(charMapA).length !== Object.keys(charMapB).length){
+        return false
+    }
+
+    for (let i in charMapA){
+        if (charMapA[i] !== charMapB[i]){
+            return false
+        }
+    }
+
+    return true
+
+
+
+    function characterMapString(str){
+
+        let shortString  = str.replace(/[&\/\\#,+()$~%.'":*?<>{} ]/g,'');
+
+
+
+        let charMap = {}
+
+        for (let char of shortString){
+            if(!charMap[char]){
+                charMap[char]=1
+            } else {
+                charMap[char]++
+            }
+        }
+
+        return charMap;
+
+
+
+    }
+    
+
+
+
+
+}
+
+module.exports = anagrams;
+
+
+
+/*
+let sortedStringA = shortenSortedString(stringA);
     let sortedStringB = shortenSortedString(stringB);
     
     if (sortedStringA.length !== sortedStringB.length){
@@ -33,6 +84,6 @@ function anagrams(stringA, stringB) {
     return sortedString;
 
     }
-}
 
-module.exports = anagrams;
+
+*/
